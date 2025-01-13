4c00ebbd44f5c31012b08d74d0f5bcf6f1f108a8
admin <levanen.n@stjolorient.fr>
2025-01-13 11:36:46 +0100

    ajout de la fonction maximum

--- C/math.c
+++ C/math.c
@@ -4,7 +4,8 @@
 /****************** Recherche du maximum ***********************/
 float maximum (float n1, float n2 )
 {
-   return 0;
+	
+   return (n1>n2) ? n1:n2;
 }
 
 /****************** Recherche du minimum ***********************/
