387e78033df42fff4cd043f471bb5fe70a96f567
admin <levanen.n@stjolorient.fr>
2025-01-13 11:41:11 +0100

    ajout fonction diff

--- Python/file_utils.py
+++ Python/file_utils.py
@@ -2,7 +2,12 @@
 
 def diff(file_first, file_second):
     """Fonction retournant True si deux fichiers sont différents."""
-    return True
+    resultat = False
+    with open(file_first) as file1_id:
+		with open(file_second) as file2_id:
+			resultat = file1_id.read() != file2_id.read() !=
+
+    return resultat
 
 def same(file_first, file_second):
     """Fonction retournant True si deux fichiers sont identiques."""
