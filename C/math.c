07277c0852e516596245a97c1650de116f1c94c8
admin <levanen.n@stjolorient.fr>
2025-01-13 11:43:21 +0100

    ajout minimum et addition

--- C/math.c
+++ C/math.c
@@ -11,12 +11,12 @@ float maximum (float n1, float n2 )
 /****************** Recherche du minimum ***********************/
 float minimum (float n1, float n2 )
 {
-   return 0;
+   return (n1<n2) ? n2:n1;
 }
 
 /******************* Addition de nombres ***********************/
 float addition (float n1, float n2 )
 {
-   return 0;
+   return n1+n2;
 }
 
